2b8b5d68821be58a3e268f8e19cc452f02c0fced
Saul Paredes <saulkurosaki13@gmail.com>
2023-12-12 13:05:53 -0600

    update about page description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,11 +19,11 @@ const About = () => {
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         I&apos;m a skilled Front-End web developer with experience in JavaScript
-        and Typescript, and expertise in technologies like React, Node.js,
-        Tailwindcss and Api&apos;s. I&apos;m a quick learner and a committed
-        developer to work hard enough and create efficient, scalable, and
-        user-friendly solutions that solve real-world problems. Let&apos;s work
-        together to bring your ideas to life!
+        and Typescript, and expertise in technologies like React, NextJs,
+        Tailwindcss and Api&apos;s management. I&apos;m a quick learner and a
+        committed developer to work hard enough and create efficient, scalable,
+        and user-friendly solutions that solve real-world problems. Let&apos;s
+        work together to bring your ideas to life!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
